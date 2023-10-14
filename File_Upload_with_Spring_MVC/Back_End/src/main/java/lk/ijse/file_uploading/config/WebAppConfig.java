e6b8abce9dfa29f4c04e71991573ba80a07da957
Udith_Dev_Wee <daveudith@gmail.com>
2023-10-14 23:51:39 +0530

    update controller class

--- File_Upload_with_Spring_MVC/Back_End/src/main/java/lk/ijse/file_uploading/config/WebAppConfig.java
+++ File_Upload_with_Spring_MVC/Back_End/src/main/java/lk/ijse/file_uploading/config/WebAppConfig.java
@@ -1,5 +1,7 @@
 package lk.ijse.file_uploading.config;
 
+import lk.ijse.file_uploading.controller.FileUploadController;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //child Context eka
 @Configuration
 @EnableWebMvc
+@ComponentScan(basePackageClasses = {FileUploadController.class})
 public class WebAppConfig implements WebMvcConfigurer {
     public WebAppConfig() {
         System.out.println("child Context");
---
+++ File_Upload_with_Spring_MVC/Back_End/src/main/java/lk/ijse/file_uploading/controller/FileUploadController.java
@@ -0,0 +1,27 @@
+package lk.ijse.file_uploading.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/upload")
+public class FileUploadController {
+
+    public FileUploadController() {
+        System.out.println("Hi I am file uploader ");
+    }
+
+    @PostMapping
+    public String uploadFile() {
+        return "post method invoked";
+    }
+
+    @GetMapping
+    public String getAllDriver() {
+        return "Get method Invoked";
+    }
+
+
+}

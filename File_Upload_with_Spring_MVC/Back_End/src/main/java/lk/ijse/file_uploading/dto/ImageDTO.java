468cdf787ba690a4ab35d21bc117134e8061b107
Udith_Dev_Wee <daveudith@gmail.com>
2023-10-14 23:59:01 +0530

    create DTO file

---
+++ File_Upload_with_Spring_MVC/Back_End/src/main/java/lk/ijse/file_uploading/dto/ImageDTO.java
@@ -0,0 +1,14 @@
+package lk.ijse.file_uploading.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class ImageDTO {
+    private String id;
+}

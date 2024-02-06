a09bd08aea87c6dfc226f8d70847ac848a8c281a
Udith_Dev_Wee <daveudith@gmail.com>
2024-02-06 11:34:39 +0530

    update FileUploadcontroller file in fromtend

--- File_Upload_with_Spring_MVC/Front_End/controller/FileUploaderController.js
+++ File_Upload_with_Spring_MVC/Front_End/controller/FileUploaderController.js
@@ -1,7 +1,40 @@
+let baseUrl="http://localhost:8082/Back_End_war/"
 $("#btnUpload").click(function () {
-    new FormData($("#txtForm")[0]);
+    let formData=new FormData($("#txtForm")[0]);
 
     $.ajax({
+        url:baseUrl+"uploads",
+        method:'post',
+        async:true,
+        contentType:false,
+        processData: false,
+        data:formData,
+        success:function (resp){
+            alert("Successfully upload");
+            imageLoad();
+        },
+        error:function (err){
+            console.log(err);
+        }
+    });
+});
 
+imageLoad();
+
+function imageLoad(){
+    $.ajax({
+        url: baseUrl+"upload/lastImageLoad",
+        method: 'get',
+        success:function (resp){
+            console.log(resp.data);
+            url1=resp.data
+            $("#display").css({
+                "background":`url(${baseUrl+url1})`,"background-size":"cover"
+            });
+        },
+
+        error:function (err){
+            console.log(err);
+        }
     })
-})
+}
